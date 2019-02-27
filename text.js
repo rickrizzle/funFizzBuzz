0f785c3b08bdb91be08f4f3a5742c9b9c6410a04
rickrizzle <rickh39@gmail.com>
2019-02-27 11:15:34 -0500

    Add chessboard problem answer

--- text.js
+++ text.js
@@ -1,8 +1,17 @@
-// FizzBuzz
+// Chessboard
 
-for (let i = 1; i <= 100; i++) {
-  let output = "";
-  if (i % 3 == 0) output += "Fizz";
-  if (i % 5 == 0) output += "Buzz";
-  console.log(output || i);
+let size = 8;
+let board = "";
+
+for (let y = 0; y < size; y++) {
+  for (let x = 0; x < size; x++) {
+    if ((x + y) % 2 == 0) {
+      board += " ";
+    } else {
+      board += "#";
+    }
+  }
+  board += "\n";
 }
+
+console.log(board);
